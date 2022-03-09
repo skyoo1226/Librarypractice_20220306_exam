7614bdff4e0d80f04016d457398d5e082b4ed931
skyoo1226 <you1226@naver.com>
2022-03-10 06:08:30 +0900

    이미지뷰를 클릭하면 새 액티비티로(ViewPhotoActivity)로 이동하여  - 사진을 크게 보는 화면

--- app/src/main/java/com/skyoo/librarypractice_20220306_exam/MainActivity.kt
+++ app/src/main/java/com/skyoo/librarypractice_20220306_exam/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.skyoo.librarypractice_20220306_exam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.time.Instant
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setEvents()
+        setValues()
+    }
+
+    fun setEvents() {
+
+        imgProfile.setOnClickListener {
+
+//            사진 크게 보는 화면으로 넘어가자. 단, 그전에 ViewPhotoActivity.kt라는 액티비티를 만들어야 함.
+            val myIntent = Intent(this, ViewPhotoActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
+    }
+
+    fun setValues() {
+
     }
 }
