dfe34c6752a7a22724b3228317970c60629dc98b
skyoo1226 <you1226@naver.com>
2022-03-10 12:35:37 +0900

    Lottie 라이브러리(애니메이션)를 위해 스플래쉬 화면을 추가 함.

---
+++ app/src/main/java/com/skyoo/librarypractice_20220306_exam/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.skyoo.librarypractice_20220306_exam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvent()
+        setValues()
+    }
+
+    fun setupEvent() {
+
+    }
+
+    fun setValues() {
+
+    }
+}
